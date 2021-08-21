41f0935aadc163589cc75aef2954a36867554f0f
Efrain Hernandez <efrainhg2050@gmail.com>
2021-08-21 17:55:41 -0500

    count down function

--- src/components/CounterDown/CounterDown.js
+++ src/components/CounterDown/CounterDown.js
@@ -3,6 +3,20 @@ import React from 'react';
 import './style.css';
 
 export const CounterDown = () => {
+
+    const dateCountDown =  new Date("Jan 5, 2022 15:37:25").getTime();
+
+    const TimeForDate  = () => {
+        const dayNow = new Date().getTime();
+
+        const distance = dateCountDown - dayNow;
+
+        const days =  Math.floor( distance / (1000 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    }
+ 
     return (
         <div className="bg-container">
             <section  className="counterdown-wrapper">
